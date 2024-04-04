2f546d2594f77985531fc88e5d66fe0147b3e0c4
MkshSahani <msco18333@gmail.com>
2024-04-05 01:31:18 +0530

    image upload api created

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
-from services.attdance_system import find_faces 
+from flask import Flask, request, flash
+from werkzeug.utils import secure_filename
 
-print(find_faces("iitism", "bss", "group.jpeg"))
+
+
+app = Flask(__name__)
+
+@app.route('/upload', methods = ['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return {}
+        file = request.files['file']
+        content = file.read()
+        f = open(file.filename, 'wb')
+        f.write(content)
+        f.close()
+        print(content)
+        return {"saved": "Ok"}
+app.run()
